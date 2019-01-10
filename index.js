675fbfb32008f149c0dadc2a46be9212447a588b
Emmanuel Anzele <eanzele@gmail.com>
2019-01-10 20:47:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ function nowServing(line) {
 }
 
 function currentLine (katzDeliLine){
-  if (katzDeliLine.length===0)
+  if (katzDeliLine.length===0) {
   return `The line is currently empty.`;
-  else
+  } else {
   return `The line is currently: 1. ${katzDeliLine[0]}, 2. ${katzDeliLine[1]}, 3. ${katzDeliLine[2]}`;
 } 
-
+}
